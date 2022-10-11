367ce1385bcd8c97076fb931ff7de237ecab7fec
kiss.dominik <kiss.dominik@ckik.hu>
2022-10-11 08:06:50 +0200

    hazi

---
+++ Python/if függvény/mire_gondolt__a_kolto.py
@@ -0,0 +1,17 @@
+p1 = int(input("irásbeli 1 pontjai: "))
+p2 = int(input("irásbeli 2 pontjai: "))
+p3 = int(input("irásbeli 3 pontjai: "))
+p4 = int(input("szóbeli pontjai: "))
+
+if p1 >= 0 and p1 <= 50 and p2 >= 0 and p2 <= 50 and p3 >= 0 and p3 <= 50 and p4 >= 0 and p4 <= 50:
+    if p4 >= 50 * 0.6:
+        if p1 >= 50 * 0.8 or p2 >= 50 * 0.8 or p3 >= 50 * 0.8:
+            print("átment")
+        else:
+            print("Bukott")
+    else:
+        print("Bukott")
+        exit()
+else:
+    print("nem lehetséges pontszám")
+    exit()         
---
+++ Python/if függvény/siknegyed.py
@@ -0,0 +1,13 @@
+x = float(input("x koordináta: "))
+y = float(input("y koordináta: "))
+
+if x > 0 and y > 0:
+    print(1)
+elif x < 0 and y > 0:
+    print(2)
+elif x < 0 and y < 0:
+    print(3)
+elif x > 0 and y < 0:
+    print(4)
+else:
+    print("vonalon van")
