655cff26f0dd1522f97b45147d9587ae6ba6c66e
kiss.dominik <kiss.dominik@ckik.hu>
2022-10-26 10:36:22 +0200

    2022.10.26

--- Python/Ciklusok/Gyakorlás/primszam.py
+++ Python/Ciklusok/Gyakorlás/primszam.py
@@ -1,6 +1,3 @@
-from codecs import namereplace_errors
-
-
 n = int(input("n: "))
 i = 2
 prim = True
@@ -8,9 +5,10 @@ prim = True
 while i < n and prim:
     if n % i == 0:
         prim = False
+        print(i)
     i += 1
 
 if prim:
     print("primszam")
 else:
-    print("nem prim")
+    print("nem prim", i-1)
---
+++ Python/Ciklusok/Gyakorlás/primszam2.py
@@ -0,0 +1,19 @@
+m = int(input("n: "))
+j = 2
+while j <= m:
+    n = j
+    i = 2
+    prim = True
+
+    while i < n and prim:
+        if n % i == 0:
+            prim = False
+            print(i)
+        i += 1
+
+    if prim:
+        print(j, end=" ")
+    else:
+        print(j, end=" ")
+
+    i += 1
