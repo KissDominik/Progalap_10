cdae4e9b2a9ce63221ac714dd79ef72117deab35
kiss.dominik <kiss.dominik@ckik.hu>
2023-01-25 17:02:46 +0100

    2023.01.15 házi

---
+++ Python/órai feladatok/függvények, eljárások/kiss_dominik_nagyok.py
@@ -0,0 +1,20 @@
+from random import *
+
+def kiválogatás(l = list, k = int):
+    lista = []
+    for i in range(len(l)):
+        if T(l[i]):
+            lista.append(i+1)
+     return lista
+
+def T(a):
+    return a > k
+
+szamok = []
+for i in range(12):
+    szamok.append(randrange(30))
+print(szamok)
+
+k = int(input("k: "))
+
+print(kiválogatás(szamok, k))
